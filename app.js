e74f4b5c63bbb6995e0f7b07f69cffc7dd6d72e5
bando <bando7507@gmail.com>
2022-08-10 23:29:20 +0100

    finalisation

--- app.js
+++ app.js
@@ -33,9 +33,7 @@ var newSwiper = new Swiper(".new-swiper", {
     slidesPerView: 'auto',
     centeredSlides: true,
     loop: 'true', 
-        // pagination: {
-        //   el: ".swiper-pagination",
-        // },
+  
       });
 
 
@@ -49,5 +47,59 @@ window.addEventListener('scroll', () =>{
     }
 })
 
+/*********script complique */
+
+// const sections = document.querySelectorAll('section[id]')
+
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
+
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight,
+//               sectionTop = current.offsetTop - 58,
+//               sectionId = current.getAttribute('id')
+
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', () =>{
+    const scrollY = window.pageYOffset
+    // console.log(scrollY)
+
+    sections.forEach(current =>{
+        // console.log(current)
+// "offsetHeight" Propriété DHTML permettant d'obtenir la hauteur d'un élément par rapport à la mise en page.
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute('id')
 
 
+         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__item').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__item').classList.remove('active-link')
+
+
+            
+        }
+    })
+})
+
+
+/**************up scrooll */
+
+ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
